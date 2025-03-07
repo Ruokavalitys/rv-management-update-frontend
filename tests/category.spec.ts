2d87be114af1e78da9c2be38588518bcd0bdf5d2
Juho Kemppainen <jhmkemppainen16@gmail.com>
2025-03-07 09:41:50 +0200

    Fix gategory tests

--- tests/category.spec.ts
+++ tests/category.spec.ts
@@ -1,13 +1,15 @@
 import { expect, test } from "@playwright/test";
 import { login } from "./fixtures/login";
-import { getRandomName } from "./utils/random";
+import { getRandomBarcode, getRandomName } from "./utils/random";
 
+let randomBarcode: string;
 let randomCategoryName: string;
-let randomRenamedCategory: string;
+let randomName: string;
 
 test.beforeAll(async () => {
+  randomBarcode = await getRandomBarcode();
   randomCategoryName = await getRandomName();
-  randomRenamedCategory = await getRandomName();
+  randomName = await getRandomName();
 });
 
 test.beforeEach(async ({ page }) => {
@@ -57,5 +59,74 @@ test.describe.serial("CRUD", () => {
     await confirmDeleteButton.click();
 
     await expect(categoryRow).not.toBeVisible();
+  });
+
+  test("When user deletes a category the product moves to default category", async ({
+    page,
+  }) => {
+    const TestcategoryName = "This category will be deleted";
+    await page.fill('input[placeholder="New Category"]', TestcategoryName);
+
+    await page.click('button:has-text("Create")');
+
+    const toast = page.locator(
+      'span[role="status"]:has-text(\'Category "This category will be deleted" has been created\')',
+    );
+    await toast.waitFor({ state: "visible" });
+
+    await expect(toast).toContainText(
+      `Category "${TestcategoryName}" has been created`,
+    );
+
+    await page.goto("/admin/new/product");
+
+    await page.getByPlaceholder("Barcode").fill(randomBarcode);
+    await page.getByPlaceholder("Barcode").press("Enter");
+    await page.getByPlaceholder("Name").fill(randomName);
+    await page.getByText("Select category").click();
+    await page.getByLabel(TestcategoryName).click();
+    await page.getByRole("button", { name: "Create Product" }).click();
+
+    await page.waitForURL(`/admin/products/${randomBarcode}`);
+
+    await expect(
+      page.locator("h1").filter({ hasText: randomName }),
+    ).toBeVisible();
+    await expect(page.locator("#category")).toHaveText(TestcategoryName);
+    await expect(page.getByLabel("Barcode")).toHaveText(randomBarcode);
+
+    await page.goto("/admin/categories");
+
+    const categoryRow = page
+      .locator("form")
+      .filter({ hasText: TestcategoryName })
+      .first();
+    await expect(categoryRow).toBeVisible();
+
+    await categoryRow.hover();
+
+    const deleteButton = categoryRow.locator('button:has-text("Delete")');
+    await deleteButton.waitFor({ state: "visible" });
+    await deleteButton.click();
+
+    const confirmDeleteButton = page
+      .locator('button:has-text("Delete")')
+      .last();
+    await confirmDeleteButton.waitFor({ state: "visible" });
+    await confirmDeleteButton.click();
+
+    await expect(categoryRow).not.toBeVisible();
+
+    const toaster = page.locator(
+      "span[role=\"status\"]:has-text('Moved 1 products to the default category')",
+    );
+    await toaster.waitFor({ state: "visible" });
+
+    await expect(toaster).toContainText(`Category deleted`);
+
+    await page.goto(`/admin/products/${randomBarcode}`);
+    await expect(page.locator("#category")).toHaveText(
+      "DEFAULT GROUP, NO DEFINITION",
+    );
   });
 });
