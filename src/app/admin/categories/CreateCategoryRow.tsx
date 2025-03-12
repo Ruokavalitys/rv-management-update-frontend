a3df5f55b1a9fd06c9b62f41e96f88f83ca439a3
Juho Kemppainen <jhmkemppainen16@gmail.com>
2025-03-12 09:18:40 +0200

    Improve category styles, spacing, and other category improvements

--- src/app/admin/categories/CreateCategoryRow.tsx
+++ src/app/admin/categories/CreateCategoryRow.tsx
@@ -7,60 +7,64 @@ import { useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 
 export const CreateCategoryRow = () => {
-  const [value, setValue] = useState("");
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
+	const [value, setValue] = useState("");
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
 
-  const [state, createCategory] = useFormState<
-    ReturnType<typeof createCategoryAction>,
-    FormData
-  >(createCategoryAction, { success: false });
+	const [state, createCategory] = useFormState<
+		ReturnType<typeof createCategoryAction>,
+		FormData
+	>(createCategoryAction, { success: false });
 
-  const { toast } = useToast();
-  useEffect(() => {
-    if (state.success && state.createdCategory) {
-      setValue("");
-      toast({
-        title: "Category Created",
-        description: `Category "${state.createdCategory.description}" has been created`,
-        duration: 6000,
-      });
-    }
-  }, [state.success, state.createdCategory]);
+	const { toast } = useToast();
 
-  useEffect(() => {
-    if (state.error) {
-      toast({
-        title: "Failed to create category",
-        description: "Please try again",
-        duration: 6000,
-      });
-    }
-  }, [state.error]);
+	useEffect(() => {
+		if (state.success && state.createdCategory) {
+			setValue("");
+			toast({
+				title: "Category Created",
+				description: `Category "${state.createdCategory.description}" has been created`,
+				duration: 6000,
+			});
+		}
+	}, [state.success, state.createdCategory]);
 
-  return (
-    <div className="group sticky bottom-0 flex items-center justify-start border-t border-gray-200 bg-white p-4">
-      <form className="ml-24 flex flex-grow items-center gap-x-2 text-lg">
-        <input
-          type="text"
-          data-next="createCategory"
-          className="w-full rounded-md border border-gray-200 p-2"
-          placeholder="New Category"
-          name="description"
-          value={value}
-          onChange={onChange}
-        />
-        <SubmitButton
-          type="submit"
-          id="createCategory"
-          formAction={createCategory}
-          variant="green"
-          disabled={value.length === 0}
-        >
-          Create
-        </SubmitButton>
-      </form>
-    </div>
-  );
+	useEffect(() => {
+		if (state.error) {
+			toast({
+				title: "Failed to create category",
+				description: "Please try again",
+				duration: 6000,
+			});
+		}
+	}, [state.error]);
+
+	return (
+		<div className="sticky bottom-0 flex items-center justify-start border-t border-gray-200 bg-white p-2 shadow-lg">
+			<form className="flex items-center w-full">
+				<div className="flex w-full pl-[200px]">
+					<input
+						type="text"
+						data-next="createCategory"
+						className="h-12 w-full border-0 bg-white py-2 px-4 text-lg font-normal text-gray-700 placeholder-gray-400 placeholder:text-lg focus:outline-none focus:ring-0 focus:border-b-[0.5px] focus:border-gray-300 transition-all duration-300 mr-4"
+						placeholder="New category"
+						name="description"
+						value={value}
+						onChange={onChange}
+					/>
+				</div>
+				<SubmitButton
+					type="submit"
+					id="createCategory"
+					formAction={createCategory}
+					variant="green"
+					disabled={value.trim().length === 0}
+					className="h-9 px-3 py-1.5 bg-green-700 text-stone-50 rounded shadow-md hover:bg-green-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition-colors duration-300 ease-in-out"
+				>
+					Create category
+				</SubmitButton>
+			</form>
+		</div>
+	);
 };
--- src/server/requests/categoryRequests.ts
+++ src/server/requests/categoryRequests.ts
@@ -8,77 +8,79 @@ const targetUrl = "api/v1/categories";
 const targetAdminUrl = "api/v1/admin/categories";
 
 export type Category = {
-  categoryId: number;
-  description: string;
+	categoryId: number;
+	description: string;
 };
 
 type categoriesRequest = {
-  categories: Category[];
+	categories: Category[];
 };
 
 export async function getAllCategories() {
-  "use server";
+	"use server";
 
-  return await authenticated<categoriesRequest>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}`,
-    {
-      method: "GET",
-      next: {
-        tags: [QueryKeys.categories],
-      },
-    },
-  ).then((data) => data.categories);
+	const categories = await authenticated<categoriesRequest>(
+		`${process.env.RV_BACKEND_URL}/${targetUrl}`,
+		{
+			method: "GET",
+			next: {
+				tags: [QueryKeys.categories],
+			},
+		},
+	).then((data) => data.categories);
+
+	return categories.sort((a, b) => a.categoryId - b.categoryId);
 }
 
 type updateCategoryRequest = Category;
 type updateCategoryResponse = {
-  category: Category;
+	category: Category;
 };
 
 export async function updateCategory(category: updateCategoryRequest) {
-  "use server";
+	"use server";
 
-  return await authenticated<updateCategoryResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetAdminUrl}/${category.categoryId}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify(category),
-    },
-  ).then((data) => data.category);
+	return await authenticated<updateCategoryResponse>(
+		`${process.env.RV_BACKEND_URL}/${targetAdminUrl}/${category.categoryId}`,
+		{
+			method: "PATCH",
+			body: JSON.stringify(category),
+		},
+	).then((data) => data.category);
 }
 
 type deleteCategoryResponse = {
-  deletedCategory: Category;
-  movedProducts: Product["barcode"][];
+	deletedCategory: Category;
+	movedProducts: Product["barcode"][];
 };
 
 export async function deleteCategory(categoryId: number) {
-  "use server";
+	"use server";
 
-  return await authenticated<deleteCategoryResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetAdminUrl}/${categoryId}`,
-    {
-      method: "DELETE",
-    },
-  );
+	return await authenticated<deleteCategoryResponse>(
+		`${process.env.RV_BACKEND_URL}/${targetAdminUrl}/${categoryId}`,
+		{
+			method: "DELETE",
+		},
+	);
 }
 
 type createCategoryRequest = {
-  description: string;
+	description: string;
 };
 
 type createCategoryResponse = {
-  category: Category;
+	category: Category;
 };
 
 export async function createCategory(category: createCategoryRequest) {
-  "use server";
+	"use server";
 
-  return await authenticated<createCategoryResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetAdminUrl}`,
-    {
-      method: "POST",
-      body: JSON.stringify(category),
-    },
-  ).then((data) => data.category);
+	return await authenticated<createCategoryResponse>(
+		`${process.env.RV_BACKEND_URL}/${targetAdminUrl}`,
+		{
+			method: "POST",
+			body: JSON.stringify(category),
+		},
+	).then((data) => data.category);
 }
