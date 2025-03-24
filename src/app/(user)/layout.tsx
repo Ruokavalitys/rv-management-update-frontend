603922c90fa28a3a0137a6a4d1531d936be5b466
Matt <matt.@Matts-MacBook-Pro.local>
2025-03-24 03:21:44 +0200

    user can login with products list and history

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,16 @@
+import AdminNavigation from "./nav";
+
+export default function AdminLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<main className="flex min-h-screen">
+			<AdminNavigation />
+			<div className="h-screen w-full rounded-l-2xl border border-stone-300 bg-white px-10 shadow-xl">
+				{children}
+			</div>
+		</main>
+	);
+}
--- src/server/requests/productRequests.ts
+++ src/server/requests/productRequests.ts
@@ -3,123 +3,140 @@
 import { authenticated } from "../wrappers";
 import { QueryKeys } from "./queryKeys";
 
-const targetUrl = "api/v1/admin/products";
+const adminTargetUrl = "api/v1/admin/products";
+const userTargetUrl = "api/v1/products";
 
-export type Product = {
-  barcode: string;
-  name: string;
-  category: {
-    categoryId: number;
-    description: string;
-  };
-  sellPrice: number;
-  stock: number;
-  buyPrice: number;
+export type AdminProduct = {
+	barcode: string;
+	name: string;
+	category: {
+		categoryId: number;
+		description: string;
+	};
+	sellPrice: number;
+	stock: number;
+	buyPrice: number;
+};
+
+export type UserProduct = {
+	barcode: string;
+	name: string;
+	category: {
+		categoryId: number;
+		description: string;
+	};
+	sellPrice: number;
+	stock: number;
 };
 
 export type getAllProductsResponse = {
-  products: Product[];
+	products: AdminProduct[];
 };
 
 export async function getAllProducts() {
-  return await authenticated<getAllProductsResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}`,
-    {
-      method: "GET",
-    },
-  ).then((data) => data.products);
+	return await authenticated<getAllProductsResponse>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}`,
+		{ method: "GET" },
+	).then((data) => data.products);
+}
+
+export type getAllProductsForUserResponse = {
+	products: UserProduct[];
+};
+
+export async function getAllProductsForUser() {
+	return await authenticated<getAllProductsForUserResponse>(
+		`${process.env.RV_BACKEND_URL}/${userTargetUrl}`,
+		{ method: "GET" },
+	).then((data) => data.products);
 }
 
 export async function getProduct(barcode: string) {
-  return await authenticated<{ product: Product }>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}/${barcode}`,
-    {
-      method: "GET",
-      next: {
-        tags: [QueryKeys.products],
-      },
-    },
-  ).then((data) => data.product);
+	return await authenticated<{ product: AdminProduct }>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}/${barcode}`,
+		{
+			method: "GET",
+			next: { tags: [QueryKeys.products] },
+		},
+	).then((data) => data.product);
 }
 
 export type addProductRequest = {
-  barcode: string;
-  name: string;
-  categoryId: number;
-  buyPrice: number;
-  sellPrice: number;
-  stock: number;
+	barcode: string;
+	name: string;
+	categoryId: number;
+	buyPrice: number;
+	sellPrice: number;
+	stock: number;
 };
 
 type addProductResponse = {
-  product: Product;
+	product: AdminProduct;
 };
 
 export const addProduct = (product: addProductRequest) => {
-  return authenticated<addProductResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}`,
-    {
-      method: "POST",
-    },
-    product,
-  ).then((data) => data.product);
+	return authenticated<addProductResponse>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}`,
+		{
+			method: "POST",
+		},
+		product,
+	).then((data) => data.product);
 };
 
 type updateProductRequest = {
-  barcode: string;
-  name?: string;
-  categoryId?: number;
-  buyPrice?: number;
-  sellPrice?: number;
-  stock?: number;
+	barcode: string;
+	name?: string;
+	categoryId?: number;
+	buyPrice?: number;
+	sellPrice?: number;
+	stock?: number;
 };
 
 type updateProductResponse = {
-  product: Product;
+	product: AdminProduct;
 };
 
 export const updateProduct = (product: updateProductRequest) => {
-  return authenticated<updateProductResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}/${product.barcode}`,
-    {
-      method: "PATCH",
-    },
-    { ...product, barcode: undefined },
-  ).then((data) => data.product);
+	return authenticated<updateProductResponse>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}/${product.barcode}`,
+		{
+			method: "PATCH",
+		},
+		{ ...product, barcode: undefined },
+	).then((data) => data.product);
 };
 
 type addStockRequest = {
-  barcode: string;
-  buyPrice: number;
-  sellPrice: number;
-  count: number;
+	barcode: string;
+	buyPrice: number;
+	sellPrice: number;
+	count: number;
 };
 
-export type addStockResponse = Omit<Product, "category" | "name">;
+export type addStockResponse = Omit<AdminProduct, "category" | "name">;
 
 export const addStock = (product: addStockRequest) => {
-  return authenticated<addStockResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}/${product.barcode}/buyIn`,
-    {
-      method: "POST",
-    },
-    {
-      count: product.count,
-      buyPrice: product.buyPrice,
-      sellPrice: product.sellPrice,
-    },
-  ).then((data) => data);
+	return authenticated<addStockResponse>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}/${product.barcode}/buyIn`,
+		{
+			method: "POST",
+		},
+		{
+			count: product.count,
+			buyPrice: product.buyPrice,
+			sellPrice: product.sellPrice,
+		},
+	).then((data) => data);
 };
 
 type deleteProductResponse = {
-  deletedProduct: Product;
+	deletedProduct: AdminProduct;
 };
 
 export const deleteProduct = (barcode: string) => {
-  return authenticated<deleteProductResponse>(
-    `${process.env.RV_BACKEND_URL}/${targetUrl}/${barcode}`,
-    {
-      method: "DELETE",
-    },
-  ).then((data) => data.deletedProduct);
+	return authenticated<deleteProductResponse>(
+		`${process.env.RV_BACKEND_URL}/${adminTargetUrl}/${barcode}`,
+		{ method: "DELETE" },
+	).then((data) => data.deletedProduct);
 };
